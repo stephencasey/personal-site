3ded4967e1f5ec5d10c5a0717ae43d0707d44dc4
Stephen Casey <stephen.thornhill.casey@gmail.com>
2025-02-16 17:20:22 -0700

    nginx website 3

--- certbot-initial/certbot-initial-entrypoint.sh
+++ certbot-initial/certbot-initial-entrypoint.sh
@@ -1,6 +1,6 @@
 certbot certonly \
         --standalone \
-        -d landscape-patterns.stephentcasey.com\
+        -d stephentcasey.com -d '*.stephentcasey.com'
         -m thornhill52320@gmail.com \
         --rsa-key-size "2048" \
         --agree-tos \
