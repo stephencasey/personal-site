4da21a896f2d7b18f7ce15a1c86bfb159d8d2e42
Stephen Casey <stephen.thornhill.casey@gmail.com>
2025-02-17 20:29:53 -0700

    cert at root dir

--- install_digitalocean.sh
+++ install_digitalocean.sh
@@ -15,8 +15,8 @@ docker compose -f ./docker-compose-initiate.yml up certbot
 docker compose -f ./docker-compose-initiate.yml down
 
 # some configurations for let's encrypt
-curl -L --create-dirs -o etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
-openssl dhparam -out etc/letsencrypt/ssl-dhparams.pem 2048
+curl -L --create-dirs -o /etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
+openssl dhparam -out /etc/letsencrypt/ssl-dhparams.pem 2048
 
 # Phase 2
 crontab ./nginx-certbot/etc/crontab
