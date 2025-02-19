d02585d3db79e988c5e9f957cf27fb33b9c9d33e
Stephen Casey <stephen.thornhill.casey@gmail.com>
2025-02-19 15:31:01 -0700

    one more script

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo '0 3  * * *  ~/personal-site-deployment/cron/cron_job.sh' | crontab -
+(crontab -l 2>/dev/null; echo '0 3  * * *  ~/personal-site-deployment/cron/cron_job.sh') | crontab -
