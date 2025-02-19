786fb5b5aac6ba143c757d38fba234a82d11e3f1
Stephen Casey <stephen.thornhill.casey@gmail.com>
2025-02-19 15:42:33 -0700

    try sudo 2

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo (crontab -l 2>/dev/null; echo '0 3  * * *  ~/personal-site-deployment/cron/cron_job.sh') | crontab -
+sudo echo '0 3  * * *  ~/personal-site-deployment/cron/cron_job.sh' | crontab -
