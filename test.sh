7c141f7a2bcd506562d01a30b10d548b10f04f35
Stephen Casey <stephen.thornhill.casey@gmail.com>
2025-02-19 16:20:31 -0700

    edit test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,3 @@
 
 # Phase 2
 echo "0 3  * * *  ~/personal-site-deployment/cron/cron_job.sh" | crontab -
-docker compose -f ./docker-compose-moondrop.yaml up -d 
-docker compose -f ./docker-compose-landscape-patterns.yaml up -d 
-docker compose -f ./docker-compose-nginx-certbot.yaml up
