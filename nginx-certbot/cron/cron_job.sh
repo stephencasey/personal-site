ff36eca6a488b5c3590d860c8b58485fa9937145
Stephen Casey <stephen.thornhill.casey@gmail.com>
2025-02-19 17:18:02 -0700

    update cron job script to use absolute path for docker-compose file

--- nginx-certbot/cron/cron_job.sh
+++ nginx-certbot/cron/cron_job.sh
@@ -10,5 +10,5 @@ else
 fi
 
 # renew certbot certificate
-docker compose -f ./docker-compose-nginx-certbot.yaml run --rm certbot
-docker compose -f ./docker-compose-nginx-certbot.yaml exec nginx nginx -s reload
+docker compose -f /home/thornhill523/personal-site-deployment/docker-compose-nginx-certbot.yaml run --rm certbot
+docker compose -f /home/thornhill523/personal-site-deployment/docker-compose-nginx-certbot.yaml exec nginx nginx -s reload
